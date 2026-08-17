3be67bbddd1bcd5c4708b5e26cfb935ac357412e
unixside <leandro.nehemias@gmail.com>
2026-08-17 20:06:32 -0300

    Minor Changes

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,56 +4,45 @@ local wezterm = require('wezterm')
 -- local theme = require('lua/rose-pine').main
 local config = wezterm.config_builder()
 
-config.font = wezterm.font "JetBrains Mono"
+config.font = wezterm.font "Aporetic Sans Mono"
 
-config.font_rules = {
-    {
-        intensity = 'Normal',
-        italic = true,
-        font = wezterm.font {
-            family = 'Iosevka NF',
-            style = 'Italic'
-        },
-    },
-}
-
-config.font_size = 13.0
+config.font_size = 15.0
 config.line_height = 1.0
 config.enable_wayland = true
-config.color_scheme = 'iTerm2 Pastel Dark Background'
+
+config.color_scheme = 'Modus-Operandi-Tinted'
+-- config.color_scheme = 'iTerm2 Pastel Dark Background'
 -- config.colors = theme.colors()
 -- config.color_scheme = 'Catppuccin Mocha'
 -- config.color_scheme = 'Black Metal (Bathory) (base16)'
-
 -- config.color_scheme = 'Modus-Vivendi-Tinted'
 -- config.color_scheme = 'Modus Vivendi Tinted (Gogh)'
 config.tab_bar_at_bottom = false
 config.use_fancy_tab_bar = false
-config.window_background_opacity = 0.9
+config.window_background_opacity = 1.0
 config.default_cursor_style = 'BlinkingBlock'
 config.animation_fps = 1
 config.cursor_blink_ease_in = 'Constant'
 config.cursor_blink_ease_out = 'Constant'
 config.cursor_blink_rate = 500
 config.adjust_window_size_when_changing_font_size = false
-config.initial_rows = 30
-config.initial_cols = 200
 config.window_decorations = " NONE "
 
 config.window_padding = {
-	left = 24,
-	right = 24,
-	top = 24,
-	bottom = 24,
+	left = 5,
+	right = 5,
+	top = 5,
+	bottom = 5,
 }
 
-wezterm.on('update-right-status', function(window, pane)
+wezterm.on('update-right-status', function(window)
 	local date = wezterm.strftime '%Y-%m-%d %H:%M:%S'
 
 	-- Make it italic and underlined
 	window:set_right_status(wezterm.format {
-		{ Attribute = { Italic = true } },
-		{ Attribute = { Intensity = "Normal" } },
+        { Attribute = { Italic = true } },
+        { Attribute = { Intensity = "Normal" } },
+        { Foreground = { Color = "#ffffff"} },
 		{ Text = date .. " " },
 	})
 end)
--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -1,12 +1,21 @@
-(setq site-run-file nil                         ; No site-wide run-time initializations. 
-      inhibit-default-init t                    ; No site-wide default library
-      gc-cons-threshold most-positive-fixnum    ; Very large threshold for garbage
-					        ; collector during init
-      package-enable-at-startup nil)                 ; We'll use straight.el
+;;; early-init.el --- -*- leaxical-binding: t -*-
+;;; Commentary:
+;;; Code:
 
+;; No site-wide run-time initializations.
+(setq site-run-file nil)
+
+;; No site-wide default library
+(setq inhibit-default-init t)
+
+;; Very large threshold for garbage collector during init
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; Define directory for native compilation
 (setq native-comp-eln-load-path
       (list (expand-file-name "eln-cache" user-emacs-directory)))
 
 ;; Reset garbage collector limit after init process has ended (8Mo)
 (add-hook 'after-init-hook
           #'(lambda () (setq gc-cons-threshold (* 8 1024 1024))))
+;;; early-init.el ends here
